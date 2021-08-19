3db205317ae620bf6c78a90bbff0d84686dd0619
Aiikon <47838475+Aiikon@users.noreply.github.com>
2021-08-18 21:01:49 -0700

    Adding tests for Set-PropertyJoinValue and Set-PropertySplitValue

---
+++ Tests/Set-PropertyJoinValue.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module $PSScriptRoot\.. -DisableNameChecking -Force
+
+Describe "Set-PropertyJoinValue" {
+
+    Context 'Default' {
+        It 'Works' {
+            [pscustomobject]@{A=1,2,3} |
+                Set-PropertyJoinValue A '+' |
+                ForEach-Object A |
+                Should Be '1+2+3'
+        }
+    }
+}
---
+++ Tests/Set-PropertySplitValue.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module $PSScriptRoot\.. -DisableNameChecking -Force
+
+Describe "Set-PropertySplitValue" {
+
+    Context 'Default' {
+        It 'Works' {
+            [pscustomobject]@{A='1+2+3'} |
+                Set-PropertySplitValue A '\+' |
+                ForEach-Object { $_.A -join '/' } |
+                Should Be '1/2/3'
+        }
+    }
+}
